823b9057a0e54fd220113d7dcb2f8328294afac5
ezuka.shunsuke <ezuka.shunsuke@sunnygiken.co.jp>
2026-01-28 12:03:52 +0900

    FatFs オリジナルコードの追加

---
+++ Sample/Standard/iMX8MPLUS/8MPLUSLPD4-EVK.A53.FATFS/src/DDR_AArch64_GICv3_cfg.h
@@ -0,0 +1,68 @@
+/**
+ * @file    DDR_AArch64_GICv3_cfg.h
+ * @brief   User configuration for ARM Generic Interrupt Controller
+ * @date    2024.09.25
+ * @author  Copyright (c) 2021-2024, eForce Co., Ltd. All rights reserved.
+ *
+ ****************************************************************************
+ * @par     History
+ *          - rev 1.0 (2021.01.25) Imada
+ *            Initial version.
+ *          - rev 1.0 (2024.09.25) Imada
+ *            Introduce the secure mode.
+ ****************************************************************************
+ */
+#ifndef DDR_AARCH64_GICV3_CFG_H_
+#define DDR_AARCH64_GICV3_CFG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "imx8mplus_uC3.h"
+#include "cpu_cfg.h"
+
+/* Priority mask for GIC CPU interface */
+#define CFG_GIC_PRI_MASK            (0xF0ULL)
+
+/* Binary point for GIC CPU interface */
+#define CFG_GIC_BIN_POINT           (3ULL)
+
+/* Base address of GIC distributor register */
+#define CFG_GIC_DIST_BASE           (GIC_BASE)
+
+/* Number of interrupts */
+#define CFG_GIC_INTNUM_MAX          (32U + 154U) /* 32 for SGI/PPI, 154 for SPI */
+
+/* Interrupt group number */
+#define G0                 0x0 /* G0: Group0 Always Secure(FIQ) */
+#define G1NS               0x1 /* G1NS: NonSecure Group1(IRQ) */
+#define G1S                0x2 /* G1S: Secure Group1(IRQ) */
+#if (CPU_ACCESS_MODE == S)
+#define CFG_GIC_INT_GRP  (G1S)
+/* G1S:Secure Group1(IRQ) mode Only */
+/*extern UW cfg_g0_array[];*/
+/*extern UW cfg_g1ns_array[];*/
+#define CFG_GIC_USE_G0   (0U)
+/* #define CFG_GIC_G0_ARRAY            (cfg_g0_array) */
+#define CFG_GIC_USE_G1NS (0U)
+/* #define CFG_GIC_G1NS_ARRAY          (cfg_g1ns_array) */
+#elif (CPU_ACCESS_MODE == NS)
+#define CFG_GIC_INT_GRP (G1NS)
+#else
+#error "Invalid CPU_ACCESS_MODE"
+#endif
+
+/* The number of CPUs using GIC */
+#define CFG_GIC_NUM_CPUS            (4U)
+
+/* The number of bits to specify GIC-500 redistributor addresses
+ * See "3.2 The GIC-500 register map" in ARM CoreLink GIC-500 Generic 
+ * Interrupt Controller Technical Reference Manual
+ * bits = 18 + max(1, ceil(log2 (total_number_of_cpus))) */
+#define CFG_GIC_NUM_BITS            (20U)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* DDR_AARCH64_GICV3_CFG_H_ */
